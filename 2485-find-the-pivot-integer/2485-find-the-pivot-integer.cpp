d8424a250e5edc1632d8c29ec06613f96e161f0a
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-03-13 10:21:27 +0530

    Time: 0 ms (100.00%), Space: 7 MB (87.40%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        for(int i=1;i<=n;i++){
-            int  left=i*(i+1)/2+i*(i-1)/2;
-            int right=n*(n+1)/2;
+//         for(int i=1;i<=n;i++){
+//             int  left=i*(i+1)/2+i*(i-1)/2;
+//             int right=n*(n+1)/2;
             
-            if(left == right) return i;
-            if(left > right) return -1;
-        }
+//             if(left == right) return i;
+//             if(left > right) return -1;
+//         }
+        
+        
+        float final = sqrt((n*(n+1))/2);
+        
+        if((int)final == final) return final;
         
         return -1;
         
     }
 };
+
+
+// (i*i + "i" + i*i-"i" )/2 = i*i;
+// i*i = n*(n+1)/2;
+// i= sqrt(n*(n+1)/2);
