d2d915ab6f9a91d70eda923f63148bf823d7e958
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-05-10 16:13:06 +0530

    Time: 59 ms (60.65%), Space: 49.4 MB (58%) - LeetHub

---
+++ 2749-promise-time-limit/2749-promise-time-limit.js
@@ -0,0 +1,39 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function (fn, t) {
+
+    // return async function (...args) {
+
+    //     const start = performance.now();
+    //     res = await fn(...args)
+    //     // res = setTimeout(() => fn(...args), t)
+    //     const diff = Math.floor(performance.now() - start);
+
+    //     console.log(res)
+    //     return await new Promise((resolve, reject) => {
+    //         if (diff < t)
+    //             resolve(res)
+    //         else reject("Time Limit Exceeded")
+    //     })
+
+    // }
+
+    //one way
+    return async function (...args) {
+        const promises = [
+            new Promise(resolve => resolve(fn(...args))), //performs task irrespective of time taken
+            new Promise((resolve, reject) => setTimeout(() => reject("Time Limit Exceeded"), t)) //reject after t millisec.
+        ]
+
+        return Promise.race(promises) //returns the fastest result
+
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
