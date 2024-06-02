1062224a61883e9bf4ae7f1149c51fa15e5a31bb
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-06-02 18:24:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,38 +1,54 @@
 class Solution {
-public:
-    
-    bool hasCycle(vector<int> graph[], int src, vector<bool> &visited, vector<bool> &dfsVisited){
-        visited[src]=true, dfsVisited[src]=true;
-        for(int i=0;i<graph[src].size();i++){
-            int child=graph[src][i];
-            if(visited[child]==false){
-                if(hasCycle(graph,child,visited,dfsVisited)) return true;
-            }else{
-                if(dfsVisited[child]) return true;
+    bool isPossible(auto& pre, int start, int prev) {
+        int n = pre.size();
+
+        map<pair<int, int>, int> vis;
+
+        queue<pair<int, int>> q;
+        q.push({start, prev});
+
+        while (q.size()) {
+            int curr = q.front().first;
+            int pr=q.front().second;
+            q.pop();
+
+            vis[{curr, pr}] = 1;
+
+            for (int p : pre[curr]) {
+                if (p == curr)
+                    return false;
+                if (vis.find({p, prev}) != vis.end())
+                    return false;
+
+                // vis[p] = 1;
+                q.push({p, curr});
             }
         }
-        dfsVisited[src]=false;
-        return false;
-    }
-    
-    bool canFinish(int n, vector<vector<int>>& pre) {
-        vector<bool> visited(n,false), dfsVisited(n,false);
-        vector<int> graph[n];
-        
-        for(int i=0;i<pre.size();i++){
-            int u=pre[i][0], v=pre[i][1];
-            graph[v].push_back(u);
-        }
-        
-        for(int i=0;i<n;i++){
-            if(visited[i]==false){
-                if(hasCycle(graph,i,visited,dfsVisited)) return false;
-            }
-        }
-        
-        for(int i=0;i<n;i++){
-            if(visited[i]==false) return false;
-        }
+
         return true;
+    }
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> pre(numCourses);
+
+        for (auto row : prerequisites) {
+            pre[row[0]].push_back(row[1]);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            cout << i << ": ";
+            for (int p : pre[i])
+                cout << p << " ";
+            cout << endl;
+        }
+        bool ans = true;
+        for (int i = 0; i < numCourses; i++) {
+            if (!isPossible(pre, i, -1)) {
+                cout << i;
+                return false;
+            }
+        }
+        return ans;
     }
 };
