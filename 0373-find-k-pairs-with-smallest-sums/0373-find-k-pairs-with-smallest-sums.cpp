dafe2234d16428201abe91816ed0ba345d9272f5
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-06-04 21:48:35 +0530

    Time: 159 ms (94.66%), Space: 141.6 MB (37.13%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        
-        priority_queue < pair<int, vector<int> > > pq;
-        for(auto n: nums1){
-            for(auto m: nums2){
-                long long sum=n+m;
-                if(pq.size()<k){
-                    vector<int> v;
-                    v.push_back(n);
-                    v.push_back(m);
-                    pq.push({sum,v});    
-                } 
-                else if(sum<pq.top().first) {
-                    pq.pop();
-                    vector<int> v;
-                    v.push_back(n);
-                    v.push_back(m);
-                    pq.push({sum,v}); 
-                }
-                else break;
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2,
+                                       int k) {
+        int n = nums1.size();
+        int m = nums2.size();
+        int i = 0, j = 0;
+        vector<vector<int>> ans;
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+
+        for (int e : nums1)
+            pq.push({e + nums2[0], 0});
+
+        while (k-- && pq.size()) {
+            int idx2 = pq.top().second;
+            int sum = pq.top().first;
+            pq.pop();
+
+            ans.push_back({sum - nums2[idx2], nums2[idx2]});
+
+            if (idx2 + 1 < m) {
+                // sum=some ele of nums1 + nums2[idx2]
+                // new sum=(sum - nums2[idx2]) + nums2[idx2+1]
+                pq.push({sum - nums2[idx2] + nums2[idx2 + 1], idx2 + 1});
             }
         }
-        vector<vector<int>> ans;
-        while(!pq.empty()){
-            ans.push_back(pq.top().second);
-            pq.pop();
-        }
-        return ans;    
+
+        return ans;
     }
 };
