44eb62b5354a0bf3c872edf2775f9ff15536f67e
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-06-04 15:53:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -4,5 +4,18 @@
  * @return {Array}
  */
 var flat = function (arr, n) {
-    return arr.flat(n)
+    if (n == 0) return arr
+
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) { 
+            console.log(arr[i]); 
+            res = [...res, ...flat(arr[i], n - 1)]; 
+        }
+        else res.push(arr[i])
+    }
+
+    return res
+    // return arr.flat(n)
+
 };
