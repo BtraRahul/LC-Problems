d031eaf03851b70c938367d9638f1f999e74fee1
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-06-05 23:11:25 +0530

    Time: 85 ms (47.83%), Space: 12.9 MB (47.81%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,44 +1,47 @@
+
 class Solution {
     vector<int> memo;
 
 public:
-    int helper(vector<int>& nums, int i = 0) {
-        int n = nums.size();
+    // int helper(vector<int>& nums, int i = 0) {
+    //     int n = nums.size();
 
-        if (memo[i] != -1)
-            return memo[i];
-        memo[i] = 1;
-        for (int j = i + 1; j < n; j++) {
-            if (nums[j] > nums[i])
-                memo[i] = max(memo[i], 1 + helper(nums, j));
-        }
+    //     if (memo[i] != -1)
+    //         return memo[i];
+    //     memo[i] = 1;
+    //     for (int j = i + 1; j < n; j++) {
+    //         if (nums[j] > nums[i])
+    //             memo[i] = max(memo[i], 1 + helper(nums, j));
+    //     }
 
-        return memo[i];
-    }
+    //     return memo[i];
+    // }
+    // int lengthOfLIS(vector<int>& nums) {
+    //     int ans = 1, n = nums.size();
+    //     memo.resize(n, -1);
+
+    //     for (int i = 0; i < n; i++) {
+    //         ans = max(ans, helper(nums, i));
+    //     }
+
+    //     for(auto e: memo) cout<<e<<" ";
+
+    //     return ans;
+    // }
     int lengthOfLIS(vector<int>& nums) {
-        int ans = 1, n = nums.size();
-        memo.resize(n, -1);
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        int ans = 1;
 
-        for (int i = 0; i < n; i++) {
-            ans = max(ans, helper(nums, i));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] > nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                    ans = max(ans, dp[i]);
+                }
+            }
         }
 
         return ans;
     }
-    // int lengthOfLIS(vector<int>& nums) {
-    //     int n = nums.size();
-    //     vector<int> dp(n, 1);
-    //     int ans = 1;
-
-    //     for (int i = 0; i < n; i++) {
-    //         for (int j = i + 1; j < n; j++) {
-    //             if (nums[j] > nums[i]) {
-    //                 dp[i] = max(dp[i], dp[j] + 1);
-    //                 ans = max(ans, dp[i]);
-    //             }
-    //         }
-    //     }
-
-    //     return ans;
-    // }
 };
