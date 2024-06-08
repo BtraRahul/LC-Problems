7964ae9b6784c47a5fca3d5d8db6e266909be1c6
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-06-08 18:46:41 +0530

    Time: 194 ms (27.4%), Space: 139.5 MB (9.98%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -8,6 +8,56 @@ class Solution {
     // 0 23 25 31 35 42
 
 public:
+    // not the optimal sol
+    // bool checkSubarraySum(vector<int>& nums, int k) {
+    //     int i = 0, j = 0;
+    //     int n = nums.size();
+
+    //     vector<int> pre(n);
+    //     // pre[0]=0;
+    //     pre[0] = nums[0];
+
+    //     unordered_map<int, int> mp; // sum: idx
+    //     for (int i = 1; i < n; i++) {
+    //         pre[i] = nums[i] + pre[i - 1];
+    //         mp[pre[i]] = i;
+
+    //         if (!(pre[i] % k))
+    //             return true;
+
+    //         if (nums[i] == 0 && nums[i - 1] == 0)
+    //             return true;
+    //     }
+
+    //     int mult = pre.back() / k;
+
+    //     mp[pre[0]] = 0;
+
+    //     for (int i : pre) {
+
+    //         for (int m = 1; m <= mult; m++) {
+    //             int sum = i + m * k;
+
+    //             if (mp.find(sum) != mp.end()) {
+    //                 if (mp[sum] - mp[i] >= 2)
+    //                     return true;
+
+    //                 int n1 = nums[mp[i]];
+    //                 int n2 = nums[mp[sum]];
+
+    //                 if (!(n1 % k) && !(n2 % k))
+    //                     return true;
+
+    //                 return false;
+    //             }
+    //         }
+    //     }
+
+    //     return false;
+    // }
+
+    // better approach calc prefix sum, for each ele of pre -> ele%k
+    //  if found any number 2 times => return true;
     bool checkSubarraySum(vector<int>& nums, int k) {
         int i = 0, j = 0;
         int n = nums.size();
@@ -19,42 +69,35 @@ public:
         unordered_map<int, int> mp; // sum: idx
         for (int i = 1; i < n; i++) {
             pre[i] = nums[i] + pre[i - 1];
-            mp[pre[i]] = i;
-
-            if (!(pre[i] % k))
-                return true;
-
-            if (nums[i] == 0 && nums[i - 1] == 0)
-                return true;
         }
 
-        int mult = pre.back() / k;
+        for (int i = 0; i < n; i++) {
 
-        mp[pre[0]] = 0;
+            pre[i] = pre[i] % k;
+            int ele = pre[i];
+            // cout << pre[i] << " ";
 
-        // for (int i : pre)
-            // cout << i << " ";
-
-        for (int i : pre) {
-
-            for (int m = 1; m <= mult; m++) {
-                int sum = i + m * k;
-                // cout << endl << sum;
-
-                if (mp.find(sum) != mp.end() && mp[i] != mp[sum]) {
-                    if (mp[sum] - mp[i] >= 2)
-                        return true;
-
-                    int n1 = nums[mp[i]];
-                    int n2 = nums[mp[sum]];
-
-                    if (!(n1 % k) && !(n2 % k))
-                        return true;
-                    return false;
-                }
-            }
+            if (mp.find(ele) != mp.end()) {
+                if (i - mp[ele] >= 2)
+                    return true;
+            } else
+                mp[ele] = i;
         }
 
+        // for (int i = 0; i < n; i++) {
+        //     int ele = pre[i];
+
+        //     if (mp.find(ele) != mp.end()) {
+        //         if (i - mp[ele] >= 2)
+        //             return true;
+        //     } else
+        //         mp[ele] = i;
+        // }
+
+        for (int i = 0; i < n; i++) {
+            if (pre[i] == 0 && i != 0)
+                return true;
+        }
         return false;
     }
 };
