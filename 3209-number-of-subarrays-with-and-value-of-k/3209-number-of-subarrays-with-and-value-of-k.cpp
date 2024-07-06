21989899881f9105e690a9f554f9987f8e9f6454
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-07-06 22:34:08 +0530

    Time: 780 ms (25%), Space: 385.5 MB (12.5%) - LeetHub

---
+++ 3209-number-of-subarrays-with-and-value-of-k/3209-number-of-subarrays-with-and-value-of-k.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        long long count = 0;
+        for (int i = 0; i < 1e8; i++) {
+        }
+        unordered_map<long long, long long> prev;
+
+        for (int i = 0; i < n; ++i) {
+            unordered_map<long long, long long> curr;
+            curr[nums[i]] = 1;
+
+            // if already k, atleast 1 ans possible
+            if (nums[i] == k) {
+                count++;
+            }
+
+            // check agar AND karne se answer aata hai ki nahi, then update the
+            // map values with updated one
+            for (auto p : prev) {
+                long long v = p.first, f = p.second, calc = v & nums[i];
+                if (calc == k) {
+                    count += f;
+                }
+                curr[calc] += f;
+            }
+
+            prev = curr;
+        }
+
+        return count;
+    }
+};
