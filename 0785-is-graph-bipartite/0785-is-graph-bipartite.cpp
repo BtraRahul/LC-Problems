24eec8cfe5de7aa0f6b0acbe27c41f6effca3b8a
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-07-11 01:54:38 +0530

    Time: 187 ms (5.1%), Space: 34.2 MB (5.16%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,40 +1,53 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        map<int, set<int>> st;
+    bool helper(vector<vector<int>>& graph, int start) {
+        vector<set<int>> nodeSet(2);
 
-        // first set -> 0
-        // second set -> 1
+        // int set=0; //1st set is zero, next set is 1
 
-        st[0].insert(0);
-        queue<pair<int, int>> q; //<node, set>
-        q.push({0, 0});
-
-        map<int, int> vis;
+        queue<pair<int, int>> q; // set, node;
+        nodeSet[0].insert(start);    // belongs to set 0;
+        q.push({0, start});
+        unordered_map<int, int> vis;
+        vis[start] = 1;
 
         while (q.size()) {
-            pair<int, int> f = q.front();
-            int currNode = f.first;
-            int nextSet = !f.second;
-
+            int currSet = q.front().first;
+            int currNode = q.front().second;
             q.pop();
+            cout << currSet << " " << currNode << endl;
 
-            if (graph[currNode].size() == 0)
-                return false;
+            int nextSet = !currSet;
+
+            // push all nodes connected to currNode to the next set. if
+            // currSet=0; next_set=1
+
             for (int node : graph[currNode]) {
-                // if node is in the same set as currNode, return false; else
-                // push
-                int currSet = f.second;
-                if (st[currSet].find(node) != st[currSet].end())
+                // if the node connected to currNode is in the same set as
+                // currNode, graph is not bipartite return false;
+                if (nodeSet[currSet].find(node) != nodeSet[currSet].end())
                     return false;
 
                 if (vis[node])
                     continue;
+
                 vis[node] = 1;
 
-                q.push({node, nextSet});
-                st[nextSet].insert(node);
+                nodeSet[nextSet].insert(node);
+                q.push({nextSet, node});
             }
+        }
+
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+
+        bool ans = true;
+        for (int i = 0; i < n; i++) {
+            ans = ans & helper(graph, i);
+            if (!ans)
+                return false;
         }
 
         return true;
