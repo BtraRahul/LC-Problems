f06cc248b679a15661977621bf303c69a22568b0
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-07-21 18:17:47 +0530

    Time: 180 ms (25%), Space: 108.5 MB (75%) - LeetHub

--- 3498-minimum-array-changes-to-make-differences-equal/3498-minimum-array-changes-to-make-differences-equal.cpp
+++ 3498-minimum-array-changes-to-make-differences-equal/3498-minimum-array-changes-to-make-differences-equal.cpp
@@ -6,7 +6,6 @@ public:
         int count = 0;
         for (int i = 0; i < n / 2; i++) {
             int a = nums[i], b = nums[n - i - 1];
-            // cout << i << " ";
             if (abs(a - b) == d)
                 continue;
 
@@ -37,7 +36,6 @@ public:
         int dif = -1;
         int f = 0;
         for (auto p : mp) {
-
             if (f < p.second) {
                 f = p.second;
                 dif = p.first;
@@ -46,28 +44,13 @@ public:
         }
 
         sort(diff.begin(), diff.end());
-        // for (int d : diff) {
-        //     // if (mp[d] == 0)
-        //     //     continue;
-        //     int val = helper(d, nums, k);
-        //     cout << d << ":" << mp[d] << " = " << val << "\n";
-        //     ans = min(ans, val);
-        // }
-
-        // calc ans for median diff
-        // sort(diff.begin(), diff.end());
 
         int size = diff.size();
-        if (size % 2) {
-            ans = min(ans, helper(diff[size / 2], nums, k));
-        } else {
-            ans = min(ans, helper(diff[size / 2], nums, k));
-            ans = min(ans, helper(diff[size / 2] + 1, nums, k));
-        }
+
+        //median
+        ans = min(ans, helper(diff[size / 2], nums, k));
 
         // ans for most freq difference
-
-        cout << dif;
         ans = min(ans, helper(dif, nums, k));
 
         return ans;
