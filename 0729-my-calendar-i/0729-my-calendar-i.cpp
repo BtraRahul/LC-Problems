0780331388dfd0330e74b9585aadf0aff81144c7
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-09-27 01:19:41 +0530

    Time: 71 ms (81.91%), Space: 42.9 MB (36.23%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,47 +1,18 @@
-// 10 - 50, 70 - 100
-// 60-80
-// 1 - 2
-// 50-70
-
-//  {10,50}, '{70,100}'   insert -> {50,70}
-//  {10,50}, {50,70}, {70,100}
-
 class MyCalendar {
-    vector<pair<int, int>> intervals;
-    int size;
+    map<int, int> intervals;
 
 public:
-    MyCalendar() { size = 0; }
+    MyCalendar() {}
 
     bool book(int start, int end) {
-        pair<int, int> newInterval = {start, end};
-        if (size == 0) {
-            size++;
-            intervals.push_back(newInterval);
-            return true;
-        }
-        int currIdx =
-            lower_bound(intervals.begin(), intervals.end(), newInterval) -
-            intervals.begin();
-
-        // currIdx  = size, -1,  0
-        pair<int, int> currInterval = {1e9, 1e9};
-        pair<int, int> prevInterval = {0, 0};
-
-        if (currIdx < size)
-            currInterval = intervals[currIdx];
-        if (currIdx - 1 >= 0)
-            prevInterval = intervals[currIdx - 1];
-
-        bool overlaps = true;
-
-        if (currInterval.first < end)
-            return false;
-        if (prevInterval.second > start)
+        auto currIdx = intervals.lower_bound(start);
+        if (currIdx != intervals.end() && currIdx->first < end)
             return false;
 
-        intervals.insert(intervals.begin()+currIdx, {start, end});
-        size++;
+        if (currIdx != intervals.begin() && prev(currIdx)->second > start)
+            return false;
+
+        intervals[start] = end;
         return true;
     }
 };
