ac55948d62754e320266339d342f31d8cf373de0
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2024-10-03 13:20:09 +0530

    Time: 42 ms (40.84%), Space: 16.2 MB (65.03%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size();
 
-        vector<int> dp(n, 1);
-        int maxAns = 1;
+        int n = nums.size(), maxAns = 1, maxCount = 0;
+        vector<int> dp(n, 1), count(n, 0);
+        count[0] = 1;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
@@ -14,8 +14,6 @@ public:
                 }
             }
         }
-        vector<int> count(n, 0);
-        count[0] = 1;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
@@ -25,18 +23,6 @@ public:
             }
             if (dp[i] == 1)
                 count[i] = 1;
-        }
-        cout << maxAns << endl;
-
-        for (int e : dp)
-            cout << e << " ";
-        cout << endl;
-        for (int e : count)
-            cout << e << " ";
-
-        int maxCount = 0;
-
-        for (int i = 0; i < n; i++) {
             if (dp[i] == maxAns)
                 maxCount += count[i];
         }
