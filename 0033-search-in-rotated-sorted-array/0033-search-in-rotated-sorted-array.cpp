813fcbaee06ac82bf7db8d912d444143952ee0e3
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2025-01-21 21:28:41 +0530

    Time: 0 ms (100%), Space: 15.1 MB (62.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,26 +1,33 @@
 class Solution {
-
-    // 1,2,3,4,5,6,7,8,9
-    // 7,8,9,1,'2',3,4,5,6   t=1;
-    // 7,'8',9,1
 public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
         int beg = 0, end = n - 1;
 
+        if (n == 1)
+            return nums[0] == target ? 0 : -1;
+
         while (beg <= end) {
             int mid = beg + (end - beg) / 2;
+            int midEle = nums[mid];
+            int leftEle = nums[beg];
+            int rightEle = nums[end];
 
-            if (nums[mid] == target)
+            if (midEle == target)
                 return mid;
-
-            if(nums[beg]<=nums[mid]){
-                if(target >= nums[beg] && target<nums[mid]) end=mid-1;
-                else beg=mid+1;
-            }else{
-                if(target > nums[mid] && target<=nums[end]) beg=mid+1;
-                else end=mid-1;
-            }         
+                
+            // look for the sorted half
+            if (leftEle <= midEle) {
+                if (leftEle <= target && target <= midEle) {
+                    end = mid - 1;
+                } else
+                    beg = mid + 1;
+            } else if (midEle <= rightEle) {
+                if (midEle <= target && target <= rightEle) {
+                    beg = mid + 1;
+                } else
+                    end = mid - 1;
+            }
         }
 
         return -1;
